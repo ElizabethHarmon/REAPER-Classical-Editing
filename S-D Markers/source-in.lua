0b343a4eb9d6cb1b57b4249f9a03bc16a8cab63e
Christopher Hampson <champson@tutanota.com>
2022-04-08 19:15:29 +0100

    Change directory name

---
+++ S-D Markers/source-in.lua
@@ -0,0 +1,29 @@
+--[[
+@noindex
+
+This file is a part of "BethHarmon_source-destination" package.
+See "BethHarmon_source-destination.lua" for more information.
+
+Copyright (C) 2022 BethHarmon
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+]]--
+
+local cur_pos = (reaper.GetPlayState() == 0) and reaper.GetCursorPosition() or reaper.GetPlayPosition()
+
+reaper.DeleteProjectMarker(NULL, 102, false)
+reaper.AddProjectMarker2(0, false, cur_pos, 0, "SOURCE-IN", 102, reaper.ColorToNative(23,223,143)|0x1000000)
+
+
+
+
