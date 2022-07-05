bf5ef887707fc3f5eab5ba4afb752d45dcb7c570
Elizabeth Harmon <betholiviaharmon@gmail.com>
2022-07-05 11:36:52 +0100

    Change name of classical tools

---
+++ classical/ReaClassical.lua
@@ -0,0 +1,37 @@
+@description ReaClassical
+@author BethHarmon
+@version 22.07.05
+@changelog
+  v22.07.05
+  Crossfade editor ripples markers & regions
+  NEW: Lock Toggle script (K)
+@metapackage
+@provides
+  [main] ReaClassical_Add Destination IN marker.lua
+  [main] ReaClassical_Add Destination OUT Marker.lua
+  [main] ReaClassical_Add Source IN marker.lua
+  [main] ReaClassical_Add Source OUT marker.lua
+  [main] ReaClassical_Classical Crossfade Editor.lua
+  [main] ReaClassical_Classical Crossfade.lua
+  [main] ReaClassical_Classical Take Record.lua
+  [main] ReaClassical_Crossfade Editor View.lua
+  [main] ReaClassical_Delete All S-D markers.lua
+  [main] ReaClassical_Duplicate folder (No items).lua
+  [main] ReaClassical_Edit Classical Crossfade.lua
+  [main] ReaClassical_Prepare Takes.lua
+  [main] ReaClassical_S-D Edit.lua
+  [main] ReaClassical_Whole Project View.lua
+  [main] ReaClassical_Create source groups (vertical).lua
+  [main] ReaClassical_Audition.lua
+  [main] ReaClassical_BirdBird Razor Children.lua
+  [main] ReaClassical_3-point edit replace.lua
+  [main] ReaClassical_Delete Leaving Silence.lua
+  [main] ReaClassical_Delete With Ripple.lua
+  [main] ReaClassical_Insert with timestretching.lua
+  [main] ReaClassical_Group Play.lua
+  [main] ReaClassical_Create Folder.lua
+  [main] ReaClassical_Lock_toggle.lua
+
+@about
+  These scripts provide dedicated source-destination markers, edits, crossfade
+  editor and more for classical music editing in REAPER.
---
+++ classical/ReaClassical_Crossfade Editor View.lua
@@ -0,0 +1,34 @@
+--[[
+@noindex
+
+This file is a part of "BethHarmon_Classical" package.
+See "BethHarmon_Classical.lua" for more information.
+
+Copyright (C) 2022 BethHarmon
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+]]
+
+local r = reaper
+r.PreventUIRefresh(1)
+r.Undo_BeginBlock()
+
+local track1 = r.NamedCommandLookup("_SWS_SEL1")
+r.Main_OnCommand(track1, 0) -- select only track 1
+r.Main_OnCommand(40113, 0) -- View: Toggle track zoom to maximum height
+local scroll_home = r.NamedCommandLookup("_XENAKIOS_TVPAGEHOME")
+r.Main_OnCommand(scroll_home, 0) -- XENAKIOS_TVPAGEHOME
+
+r.Undo_EndBlock('Crossfade Editor View', 0)
+r.PreventUIRefresh(-1)
+r.UpdateArrange()
+r.UpdateTimeline()
